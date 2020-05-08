2a376945a7f1a1a94de5894ba5fcdaf9c173f899
Dmitry Kyrychenko <kirichenkodmitryy@gmail.com>
2020-05-08 11:49:27 +0300

    Initial Commit

---
+++ WeaklyFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeaklyFinder
+//
+//  Created by Дмитрий Кириченко on 08.05.2020.
+//  Copyright © 2020 Dmitry Kirichenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
